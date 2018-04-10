bfb5be6b84b0d634f13206d8a02458d5ea10676c
megomark <megomark@gmail.com>
2018-04-10 18:12:09 +0300

    Add greatings for evertybody

--- main.go
+++ main.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 )
 
+func doSomthing() {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ) Hello men!", i)
+	}
+}
+
 func main() {
 	fmt.Println("Привет. Мир!")
 	fmt.Println("Привет. Друзья!")
 	fmt.Println("Привет. Люди!")
+	doSomthing()
 }
